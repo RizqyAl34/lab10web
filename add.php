d6fa95cba0257b89b0b76d072cf5aa84c05acb94
RizqyAl34 <rizkial347@gmail.com>
2025-12-02 21:14:30 +0700

    Add files via upload

---
+++ add.php
@@ -0,0 +1,25 @@
+<?php
+include "form.php";
+include "database.php";
+
+$db = new Database();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = [
+        'nim' => $_POST['nim'] ?? '',
+        'nama' => $_POST['nama'] ?? '',
+        'alamat' => $_POST['alamat'] ?? ''
+    ];
+    $ok = $db->insert('mahasiswa', $data);
+    if ($ok) {
+        header('Location: list.php'); exit;
+    } else {
+        echo "Gagal insert: ".$db->conn->error;
+    }
+}
+
+$form = new Form("add.php", "Tambah Data");
+$form->addField("nim", "NIM");
+$form->addField("nama", "Nama");
+$form->addField("alamat", "Alamat");
+$form->displayForm();
---
+++ delete.php
@@ -0,0 +1,9 @@
+<?php
+include "database.php";
+$db = new Database();
+$id = $_GET['id'] ?? null;
+if ($id) {
+    $ok = $db->delete('mahasiswa', "id=".intval($id));
+}
+header('Location: list.php');
+exit;
